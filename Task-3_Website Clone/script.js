685070a432a1704e44383ba776fc8dd69c735ea3
Vishal Kumar <imvgupta21@gmail.com>
2023-07-26 05:07:09 +0530

    task-3

---
+++ Task-3_Website Clone/script.js
@@ -0,0 +1,7 @@
+let dropbox = document.getElementById("menu-icon")
+console.log(dropbox)
+dropbox.addEventListener('click',() =>{
+    let box = document.getElementsByTagName("a")
+    // box.classList.toggle("toggle1")
+    console.log(box)
+})
